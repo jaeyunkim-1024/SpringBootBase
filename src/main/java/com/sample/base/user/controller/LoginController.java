8683d32e8ca2c5c3205b032d2d8fdee9d5007411
jaeyunkim-1024 <rlawodbs1024@gmail.com>
2024-09-30 20:11:14 +0900

    feat: 회원 가입,로그인,회원 탈퇴 처리
    - @RequestBody 적용

--- src/main/java/com/sample/base/user/controller/LoginController.java
+++ src/main/java/com/sample/base/user/controller/LoginController.java
@@ -7,23 +7,26 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class LoginController {
     private final LoginService loginService;
 
     @PostMapping("/login")
-    public ResponseEntity<UserDto> login(LoginRequestDto dto){
+    public ResponseEntity<UserDto> login(@RequestBody LoginRequestDto dto){
         return ResponseEntity
                 .ok()
                 .body(loginService.login(dto));
     }
 
     @PostMapping("/logout")
-    public ResponseEntity logout(LoginRequestDto dto){
+    public ResponseEntity logout(@RequestBody LoginRequestDto dto){
         return ResponseEntity
                 .ok()
                 .body(null);
